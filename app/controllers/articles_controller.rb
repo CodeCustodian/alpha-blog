fa2a78d0ec2452021c6697beb17a41d64163e41e
Everett <mail@uncleev.com>
2016-09-04 16:08:35 +0000

    prepare for prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,60 @@
 class ArticlesController < ApplicationController
+    before_action :set_article_number, only: [:edit, :update, :show, :destroy]
+    
+    
+    def index
+        @articles = Article.all
+    end
+    
     def new
         @article = Article.new
     end
     
-    def create
-        #render plain: params[:article].inspect
-        @article = Article.new(article_params) 
-        @article.save 
-        redirect_to articles_show(@article)
+    def edit
+        # @article = Article.find(params[:id]) 
+        @article = set_article_number()
+    end
+    
+    def update
+       @article = Article.find(params[:id]) 
+        if @article.update(article_params)
+           flash[:notice] = "Article was successfully Updated!"
+           redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end 
     
-    private 
-    def article_params
-       params.require(:article).permit(:title, :description)  
+    def show
+    #   @article = Article.find(params[:id]) 
+    end    
+    
+    def destroy
+    #   @art = Article.find(params[:id])  
+      @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+      redirect_to articles_path
     end
+    
+    def create
+                #render plain: params[:article].inspect
+        @article = Article.new(article_params) 
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end 
+    
+    
+    private 
+    
+        def set_article_number
+            @article = Article.find(params[:id])
+        end
+        def article_params
+            params.require(:article).permit(:title, :description)  
+        end
     
 end
